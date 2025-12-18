3023da8689356ff25d1efe62bb3971cf10f19a9e
admiralmochii <doggiejedimaster@gmail.com>
2025-12-18 14:58:44 +0800

    Apologies for the screwup, currently I will be using this branch for both Tuya LED lights and projector integration as i forgot to switch off before finishing projector integrationg.

--- server/routes/lightsrouter.mjs
+++ server/routes/lightsrouter.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import db from "../db/conn.mjs"
 import 'dotenv/config';
 import { cloudLogin, loginDevice, loginDeviceByIp } from 'tp-link-tapo-connect';
+const TuyAPI = (await import('tuyapi')).default;
 
 const tplink_user = process.env.TPLINKUSER
 const tplink_password = process.env.TPLINKPASSWORD
@@ -79,6 +80,52 @@ router.post("/shutdown/:light_name", async (req,res) => {
             details: err.message
         });
     }
+})
+
+//temp to test led lights
+
+//Access ID/Client ID: uadhheywrmevxpvk455s
+//Access Secret/Client Secret: 6d9fd7f8710f4fc4a7bc3337202b30f8
+//Project Code: p176535192086359krrj
+//local key ?!JLq(3l.P'^Ge+F
+//app schema ideasled
+
+const tuya_device = new TuyAPI({
+  id: 'a33f7fdfa3af27ca2axft3',      // Device ID (from Tuya app/platform)
+  key: "?!JLq(3l.P'^Ge+F",     // Local encryption key
+  ip: '',       // Optional: device IP
+  version: '3.3',            // Protocol version (3.1 or 3.3)
+  issueGetOnConnect: true,   // Automatically request device state on connect
+  issueRefreshOnConnect: true, // Request fresh data
+  nullPayloadOnJSONError: false // How to handle malformed JSON
+});
+
+router.post("/wakeled/:light_name", async (req,res) => {
+    const deviceName = req.params.light_name;
+
+    const light  = await collection.findOne({device_name: `${deviceName}`, category: "3"})
+
+    if (!light) {
+        return res.status(404).json({ error: 'Light device not found' });
+    }
+
+    console.log(light.ip)
+    try {
+        
+
+        res.status(200).json({
+            success: true,
+            message: `${deviceName} successfully turned on.`
+        })
+
+    } catch(err){
+        console.error( `Failed to wake ${deviceName}`,err)
+        res.status(500).json({
+            error: `Failed to wake ${deviceName}`,
+            details: err.message
+        });
+    }
+    
 })
 
 
